1df31a138aeb42a9d3fb6472582e2e2459a296ef
valeriasdiaz <sandovaldiazvm@appstate.edu>
2024-01-30 18:26:24 -0500

    adding frontend and backend folders and files

---
+++ pocado-frontend/src/HeaderComponent.js
@@ -0,0 +1,10 @@
+function HeaderComponent(){
+    return (
+        <div>
+            <header>
+                <button>click me!</button>
+            </header>
+        </div>
+    );
+}
+export default HeaderComponent;

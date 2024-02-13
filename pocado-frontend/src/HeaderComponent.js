beade3ab2ea5f69f2abc7a2ce73e035d5d63ab63
flowermininis <flowerminini@gmail.com>
2024-02-13 14:30:13 -0500

    created navigation bar for the website, also add js and css files for the sign in and sign up pages, and also added some svg icons to the navigation bar

--- pocado-frontend/src/HeaderComponent.js
+++ pocado-frontend/src/HeaderComponent.js
@@ -1,10 +1,47 @@
-function HeaderComponent(){
-    return (
-        <div>
-            <header>
-                <button>click me!</button>
-            </header>
+import "./HeaderComponent.css";
+import basket_shopping_solid from "./icons/basket_shopping_solid.svg";
+import heart_solid from "./icons/heart_solid.svg";
+
+function HeaderComponent() {
+  return (
+    <header className="header-area">
+      <div className="header-tag">
+        <div className="logo-div"></div>
+        <div className="search-div">
+          <input
+            className="search-bar"
+            placeholder="what can we help you with?"
+          ></input>
         </div>
-    );
+        <div id="buttons">
+          <ul className="button-div">
+            <li className="nav-buttons">
+              <button className="header-buttons">
+                <img id="heart-icon" src={heart_solid} />
+              </button>
+            </li>
+            <li className="nav-buttons">
+              <button className="header-buttons">
+                <img id="basket-icon" src={basket_shopping_solid} />
+              </button>
+            </li>
+            <li className="nav-buttons acc-btn">
+              <button id="signup-button" className="header-buttons acc-btn">
+                sign up
+              </button>
+            </li>
+            <li className="nav-buttons acc-btn">
+              <button id="login-button" className="header-buttons acc-btn">
+                log in
+              </button>
+            </li>
+            <li className="nav-buttons acc-btn">
+              <button className="header-buttons acc-btn">sell</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 }
 export default HeaderComponent;
---
+++ pocado-frontend/src/SignIn.js
@@ -0,0 +1,15 @@
+import "./SignIn.css";
+import HeaderComponent from "./HeaderComponent";
+
+function SignIn() {
+  return (
+    <div>
+      <HeaderComponent></HeaderComponent>
+      <div>
+        <p>please log in</p>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;

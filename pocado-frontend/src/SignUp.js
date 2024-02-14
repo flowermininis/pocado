5a23529f4db6c362eed5f7997e53fc222d1d92c7
flowermininis <flowerminini@gmail.com>
2024-02-14 03:08:35 -0500

    added more styling to sign up page, like a light gradient background, a container for the form, an image with the planned logo, and new font for the entire site hehe

--- pocado-frontend/src/SignUp.js
+++ pocado-frontend/src/SignUp.js
@@ -1,34 +1,56 @@
 import "./SignUp.css";
 import HeaderComponent from "./HeaderComponent";
+import logo_test from "./logos/logo_test.png";
 
 function SignUp() {
   return (
-    <div>
-      <HeaderComponent></HeaderComponent>
+    <body className="page-div">
+      <div className="logo-div">
+        <img id="logo-svg" src={logo_test} />
+      </div>
       <div className="signup-div">
         <div className="inner-signup">
-          <p>please sign up to continue!</p>
+          <div id="signup-mssg-div">
+            <h2>Registration</h2>
+          </div>
           <br></br>
-          <label htmlFor="email-box">email:</label>
+          <label htmlFor="email-box">Email:</label>
           <br></br>
           <input
+            className="user-input"
             type="email"
             name="email-box"
             placeholder="Your email address"
           ></input>
           <br></br>
-          <label htmlFor="un-box">username:</label>
+          <label htmlFor="un-box">Username:</label>
           <br></br>
-          <input type="text" name="un-box" placeholder="example123"></input>
+          <input
+            className="user-input"
+            type="text"
+            name="un-box"
+            placeholder="example123"
+          ></input>
           <br></br>
-          <label htmlFor="pw-box">password:</label>
+          <label htmlFor="pw-box">Password:</label>
           <br></br>
-          <input type="password" name="pw-box"></input>
+          <input className="user-input" type="password" name="pw-box"></input>
           <br></br>
-          <input type="submit"></input>
+          <div id="submit-bttn-div">
+            <input
+              className="submit-button"
+              type="submit"
+              value="Create Account"
+            ></input>
+          </div>
+          <div>
+            <h5>
+              Already have an account? <a href="/home">Log in!</a>
+            </h5>
+          </div>
         </div>
       </div>
-    </div>
+    </body>
   );
 }
 
